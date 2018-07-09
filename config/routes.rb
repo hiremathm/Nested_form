ebdcfd6147b141482fa368ca42359248a138dfdd
shivakumara m <shivasorab@gmail.com>
2018-07-09 17:34:15 +0530

    comment and article model is created

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  
+
+  resources :articles
+  resources :comments
   resources :surveys
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'surveys#index'
+   root 'articles#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180709074414) do
+ActiveRecord::Schema.define(version: 20180709104023) do
 
   create_table "answers", force: :cascade do |t|
     t.string   "context"
     t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "description"
+    t.integer  "article_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
